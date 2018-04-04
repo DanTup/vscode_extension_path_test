a14b386b634ca819d147644b8cfdf9a1cedc08c8
Danny Tuppeny <danny@tuppeny.com>
2018-04-04 18:48:51 +0100

    Log path inside test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -18,5 +18,7 @@ suite("Extension Tests", () => {
     test("Something 1", () => {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
+        console.log("INSIDE TESTS, PATH IS");
+        console.log(process.env.PATH);
     });
 });
