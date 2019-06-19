4955eee77a82f51c908944a83ba276baaadcd649
Danny Tuppeny <danny@tuppeny.com>
2019-06-19 11:59:07 +0100

    ...

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,22 +2,15 @@ import * as path from 'path';
 
 import { runTests } from 'vscode-test';
 
+
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionPath = path.resolve(__dirname, '../../');
+	console.log("Inside user runTest.ts");
+	console.log(process.env.PATH);
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const testRunnerPath = path.resolve(__dirname, '.');
+	const extensionPath = path.resolve(__dirname, '../../');
+	const testRunnerPath = path.resolve(__dirname, '.');
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionPath, testRunnerPath });
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+	await runTests({ extensionPath, testRunnerPath });
 }
 
 main();
